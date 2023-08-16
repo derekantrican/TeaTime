74dc53b26fa95dd43c4dbe722602ea59cb2357a0
Derek Antrican <derekantrican@gmail.com>
2023-08-16 10:28:18 -0700

    Fixed homepage sizing vs content pages

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { HomePage } from './pages/Homepage';
 import { Layout } from './components/Layout';
 import { Guidelines } from './pages/Guidelines';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={
-          <Layout>
+          // Using div & NavBar directly for HomePage as it needs height 100% (minHeight 100% doesn't seem to work)
+          <div style={{height: '100%'}}>
+            <NavBar />
             <HomePage/>
-          </Layout>
+          </div>
         }/>
         {/*Todo: I want this site to act as a single-page app. That's not how it's acting now and I'm not sure what to change*/}
         <Route path='/guidelines' element={
