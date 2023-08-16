55990a190038cd51ed12929a29393bc6486cfef7
Derek Antrican <derekantrican@gmail.com>
2023-08-16 11:05:41 -0700

    A couple tweaks

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export function HomePage() { 
     return (
       <header style={{ paddingLeft: 0, height: '100%' }}>
@@ -9,9 +11,9 @@ export function HomePage() {
             <div className='text-white p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.4)', borderRadius: 20 }}>
               <h1 className='mb-3'>A time for hanging out, drinking tea, playing games, and making friends</h1>
               <h4 className='mb-3'>Regardless of race, religion, culture, language level, or any other factor - all are welcome!</h4>
-              <a className='btn btn-outline-light btn-lg' style={{backgroundColor: 'rgba(130, 174, 245, 0.5)'}} href='/groups'>
+              <Link className='btn btn-outline-light btn-lg' style={{backgroundColor: 'rgba(130, 174, 245, 0.5)'}} to='/groups'>
                 Find a group near you
-              </a>
+              </Link>
             </div>
           </div>
         </div>
