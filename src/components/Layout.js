ea364c71e466d6e61ba19dbb5a8a34f6a9a00d7d
Derek Antrican <derekantrican@gmail.com>
2023-11-04 20:50:03 -0700

    Add a contact link to the bottom of most pages

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import NavBar from "./NavBar";
 
 export function Layout(props) {
@@ -5,6 +6,9 @@ export function Layout(props) {
       <div style={{minHeight: '100%', backgroundColor: 'rgba(68, 25, 11, 0.9)', color: 'white'}}>
         <NavBar />
         {props.children}
+        <div style={{display: 'flex', justifyContent: 'center', padding: 10}}>
+          <Link to='mailto:contact@tea-time.social'>Contact</Link>
+        </div>
       </div>
     );
 }
